369d6d9863bbe8b8fedc3386e8e24c6da8b19e63
Dan Abrey <danabrey@gmail.com>
2018-02-21 10:53:49 +0000

    Move widgets around

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,13 +6,13 @@
 
 <dashboard id="dashboard" columns="6" rows="3">
     <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a3"></twitter>
-    <music position="b1:d1"></music>
+    <music position="b1:c1"></music>
     <review-stats position="e1"></review-stats>
     <ad-stats position="f1"></ad-stats>
     <todos heading="Dan" team-member="In Progress - Dan" position="b2"></todos>
     <todos heading="Bloke" team-member="In Progress - Bloke" position="c2"></todos>
-    <time-weather position="e3" date-format="ddd DD/MM" time-zone="Europe/London" weather-city="Oxford, UK"></time-weather>
-    <calendar position="f2:f3"></calendar>
+    <time-weather position="d1" date-format="ddd DD/MM" time-zone="Europe/London" weather-city="Oxford, UK"></time-weather>
+    <calendar position="d2:d3"></calendar>
     <internet-connection></internet-connection>
 </dashboard>
 
