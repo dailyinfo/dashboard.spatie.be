5eae970613fb867fc9e065e206f0f9e4c346706d
Dan Abrey <danabrey@gmail.com>
2018-02-22 19:11:51 +0000

    log to errorlog

--- config/logging.php
+++ config/logging.php
@@ -32,7 +32,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['syslog'],
+            'channels' => ['errorlog'],
         ],
 
         'single' => [
